53ace8522cbf69e139fc1036622456dd60e6f168
godiyalashish <45142272+godiyalashish@users.noreply.github.com>
2019-04-14 15:11:16 +0530

    Update Navigation.js

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import './Navigation.css'
+import React from 'react';
+import 'tachyons';
 
 const Navigation = () => {
 
   return(
 
- 		<nav>
- 			<h1>Weather-App</h1>
- 		</nav>
+ 			<h1 className = "f1 f1-l fw2 white-90 mb0 lh-title" >Weather-App</h1>
 
   	);
 
